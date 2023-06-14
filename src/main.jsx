3b597d5561ed8d61424336da509c10f51cb1c9b4
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-14 11:43:46 +0600

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home.jsx';
+import Books from './components/Books.jsx';
+import About from './components/About.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/books",
+        element:<Books/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
